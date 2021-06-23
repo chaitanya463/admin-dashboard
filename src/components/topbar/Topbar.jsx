9b43124860c329598707a4919109b3e2c8f22b1d
chaitanya.ms <chaitanms@gmail.com>
2021-06-23 21:55:18 +0530

    Change topbar title

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -8,7 +8,7 @@ const Topbar = () => {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">ZuesAdmin</span>
+          <span className="logo">LG Admin</span>
         </div>
         <div className="topRight">
           
